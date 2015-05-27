99a1d66ca1cec8ee029c96d16d4ecb5854302c57
vicksonzero <dicksonzero@gmail.com>
2015-05-27 03:06:46 +0100

    basic things

---
+++ app.js
@@ -0,0 +1,21 @@
+var Promise = require("promise").Promise;
+var fs = require("fs");
+var libxmljs = require("libxmljs");
+
+
+
+
+module.exports = function(){
+	/**
+	 * Constructor
+	 * @param [string, string] inputs two strings of xml filepath
+	 */
+	function App(inputs){
+		var p = new Promise()
+			.then(function(){
+
+			});
+
+	}
+	return App;
+})();
